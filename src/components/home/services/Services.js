194f69bc947aac72f997a7d72853894c5dc66413
mjrgeorge <mjrgeorge@gmail.com>
2020-10-14 16:34:11 +0600

    just old file deleted

--- src/components/home/services/Services.js
+++ src/components/home/services/Services.js
@@ -15,25 +15,6 @@ const Services = () => {
         .then(data =>setServices(data))
         .catch(err =>console.log(err))
     }, []);
-    console.log({services});
-
-    // const serviceData = [
-    //     {
-    //         name: 'Web & Mobile design',
-    //         description: 'We craft stunning and amazing web UI, using a well drrafted UX to fit your product.',
-    //         img: service1
-    //     },
-    //     {
-    //         name: 'Graphic design',
-    //         description: 'Amazing flyers, social media posts and brand representations that would make your brand stand out.',
-    //         img: service2
-    //     },
-    //     {
-    //         name: 'Web development',
-    //         description: 'With well written codes, we build amazing apps for all platforms, mobile and web apps in general.',
-    //         img: service3
-    //     }
-    // ]
 
     return (
         <section className="mt-5 text-center">
