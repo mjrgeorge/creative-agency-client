a1c1688297b981048d5959036e9f3d370d408d19
mjrgeorge <mjrgeorge@gmail.com>
2020-10-16 23:18:41 +0600

    somthing updated

--- src/components/adminPages/servicesList/ServicesList.js
+++ src/components/adminPages/servicesList/ServicesList.js
@@ -26,39 +26,53 @@ const ServicesList = () => {
                     </div>
                     <h3 className="ml-5 pl-5 pb-5">Services List</h3>
                     {
-                        orders.length===0 ?
-                        <div>
-                            <img className="rounded mx-auto d-block p-5" src={loading} alt="Loading..."/>
-                        </div>
-                        :
-                        <div className="bg-white p-4 rounded">
-                        <div className="table-responsive-sm">
-                            <table className="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Email</th>
-                                        <th scope="col">Services</th>
-                                        <th scope="col">Project Details</th>
-                                        <th scope="col">Status</th>
-                                    </tr>
-                                </thead>
-                                {
-                                    orders.map(order =>
-                                        <tbody key={order._id}>
+                        orders.length === 0 ?
+                            <div>
+                                <img className="rounded mx-auto d-block p-5" src={loading} alt="Loading..." />
+                            </div>
+                            :
+                            <div className="bg-white p-4 rounded">
+                                <div className="table-responsive-sm">
+                                    <table className="table table-striped">
+                                        <thead>
                                             <tr>
-                                                <td>{order.name}</td>
-                                                <td>{order.email}</td>
-                                                <td>{order.service}</td>
-                                                <td>{order.details}</td>
-                                                <td>Dropdown</td>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">Services</th>
+                                                <th scope="col">Project Details</th>
+                                                <th scope="col">Status</th>
                                             </tr>
-                                        </tbody>
-                                    )
-                                }
-                            </table>
-                        </div>
-                    </div>
+                                        </thead>
+                                        {
+                                            orders.map(order =>
+                                                <tbody key={order._id}>
+                                                    <tr>
+                                                        <td>{order.name}</td>
+                                                        <td>{order.email}</td>
+                                                        <td>{order.service}</td>
+                                                        <td>{order.details}</td>
+                                                        <td>
+                                                            <div class="d-flex">
+                                                                <div class="btn-group">
+                                                                    <button type="button" class="btn btn-white">Pending</button>
+                                                                    <button type="button" class="btn btn-white dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-reference="parent">
+                                                                        <span class="sr-only">Toggle Dropdown</span>
+                                                                    </button>
+                                                                    <div style={{ cursor: "pointer" }} class="dropdown-menu" aria-labelledby="dropdownMenuReference">
+                                                                        <span className="dropdown-item">Pending</span>
+                                                                        <span className="dropdown-item">Ongoing</span>
+                                                                        <span className="dropdown-item">Done</span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            )
+                                        }
+                                    </table>
+                                </div>
+                            </div>
                     }
                 </section>
             </div>
